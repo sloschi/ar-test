16266acfa68dd0d358a7696edb694f1d9f126fe7
Sebastian Losch <seb.losch@gmail.com>
2017-12-08 11:12:27 +0100

    remove wonderland

--- js/app.js
+++ js/app.js
@@ -23,6 +23,8 @@ arToolkitSource.init(function() {
 
   var scene = new THREE.Scene();
 
+  addLights(scene);
+
   var camera = new THREE.Camera();
   scene.add(camera);
 
@@ -49,6 +51,17 @@ arToolkitSource.init(function() {
 
   onResize();
 
+  // add a torus knot
+  var geometry = new THREE.CubeGeometry(1, 1, 1);
+  var material = new THREE.MeshNormalMaterial({
+    transparent: true,
+    opacity: 0.5,
+    side: THREE.DoubleSide
+  });
+  var mesh = new THREE.Mesh(geometry, material);
+  mesh.position.y = geometry.parameters.height / 2;
+  scene.add(mesh);
+
   // update artoolkit on every frame
   onRenderFcts.push(function() {
     if (!arToolkitSource.ready) return;
@@ -66,42 +79,22 @@ arToolkitSource.init(function() {
 
   scene.visible = false;
 
-  var loader = new THREE.GLTFLoader();
+  onRenderFcts.push(function() {
+    renderer.render(scene, camera);
+  });
 
-  createWonderland(scene, loader, function(snow, snowman, tree, logo) {
-    onRenderFcts.push(function(deltaG) {
-      updateSnow(snow, deltaG);
-    });
+  var lastTimeMsec = null;
 
-    onRenderFcts.push(function(deltaG) {
-      updateSnowman(snowman, deltaG);
-    });
+  requestAnimationFrame(function animate(nowMsec) {
+    requestAnimationFrame(animate);
 
-    onRenderFcts.push(function(deltaG) {
-      updateTree(tree, deltaG);
-    });
+    lastTimeMsec = lastTimeMsec || nowMsec - 1000 / 60;
 
-    onRenderFcts.push(function(deltaG) {
-      updateLogo(logo, deltaG);
-    });
+    var deltaMsec = Math.min(200, nowMsec - lastTimeMsec);
+    lastTimeMsec = nowMsec;
 
-    onRenderFcts.push(function() {
-      renderer.render(scene, camera);
-    });
-
-    var lastTimeMsec = null;
-
-    requestAnimationFrame(function animate(nowMsec) {
-      requestAnimationFrame(animate);
-
-      lastTimeMsec = lastTimeMsec || nowMsec - 1000 / 60;
-
-      var deltaMsec = Math.min(200, nowMsec - lastTimeMsec);
-      lastTimeMsec = nowMsec;
-
-      onRenderFcts.forEach(function(onRenderFct) {
-        onRenderFct(deltaMsec / 1000, nowMsec / 1000);
-      });
+    onRenderFcts.forEach(function(onRenderFct) {
+      onRenderFct(deltaMsec / 1000, nowMsec / 1000);
     });
   });
 });
@@ -117,94 +110,4 @@ function addLights(aScene) {
 
   var light3 = new THREE.AmbientLight(0x222222);
   aScene.add(light3);
-}
-
-function addIsland(aScene, aLoader, onDone) {
-  aLoader.load("./assets/wonderland.glb", function(data) {
-    var gltf = data;
-    var island = gltf.scene;
-
-    island.scale.copy(new THREE.Vector3(2, 2, 2));
-    island.rotateY(-90);
-
-    var snowman = island.getObjectByName("snowman_rotation");
-    var tree = island.getObjectByName("tree");
-    var logo = island.getObjectByName("Logo");
-
-    aScene.add(island);
-    onDone(snowman, tree, logo);
-  });
-}
-
-function addSnow(aCount, aScene, aLoader, onDone) {
-  aLoader.load("./assets/logo.glb", function(data) {
-    var snowFlake = data.scene;
-    var material = new THREE.MeshLambertMaterial({ side: THREE.DoubleSide });
-
-    snowFlake.traverse(function(child) {
-      if (child instanceof THREE.Mesh) {
-        child.material = material;
-      }
-    });
-
-    var snow = new THREE.Group();
-    for (var i = 0; i < SNOW_COUNT; i++) {
-      var flake = snowFlake.clone();
-      flake.rotation.x = Math.random() * 6;
-      flake.rotation.y = Math.random() * 6;
-      flake.rotation.z = Math.random() * 6;
-      flake.position.copy(
-        new THREE.Vector3(
-          (Math.random() - 0.5) * 3,
-          (0.5 + Math.random() - 0.5) * 3.5,
-          (Math.random() - 0.5) * 3
-        )
-      );
-      snow.add(flake);
-    }
-
-    aScene.add(snow);
-    onDone(snow);
-  });
-}
-
-function createWonderland(aScene, aLoader, onDone) {
-  addIsland(aScene, aLoader, function(snowman, tree, logo) {
-    addSnow(SNOW_COUNT, aScene, aLoader, function(snow) {
-      addLights(aScene);
-      onDone(snow, snowman, tree, logo);
-    });
-  });
-}
-
-function updateSnow(snow, deltaG) {
-  snow.children.forEach(function(flake) {
-    flake.rotateX(deltaG);
-    flake.rotateY(deltaG);
-    flake.rotateZ(deltaG);
-    flake.position.y -= 0.2 * deltaG;
-
-    if (flake.position.y < 0) {
-      flake.position.copy(
-        new THREE.Vector3(flake.position.x, 2, flake.position.z)
-      );
-    }
-  });
-}
-
-function updateSnowman(snowman, deltaG) {
-  var limit = Math.PI / 8;
-
-  snowman.rotation.x = Math.sin(Date.now() * 0.005) * limit;
-}
-
-function updateTree(tree, deltaG) {
-  var limit = Math.PI / 20;
-
-  tree.rotation.x = Math.sin(Date.now() * 0.002) * limit;
-  tree.rotation.y += deltaG;
-}
-
-function updateLogo(logo, deltaG) {
-  logo.rotation.y += deltaG;
 }
